ede0134b5d89b137da95d3948fef052427d10944
CHUTIYA THANALUCK <122534225+mint-cty@users.noreply.github.com>
2023-02-03 09:50:00 +0000

    main

--- main.c
+++ main.c
@@ -1,19 +1,46 @@
-#define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
-
-   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
-
-  // selectionSort(a,N);
-   display(a,N);
- return 0;
+int Isprime(int num){
+  if(num==1) return 0;
+  for(int i=2; i<num; i++){
+    if(num%i==0) return 0;
+  }
+ return 1;
 }
+
+int main(int argc , char *argv[]) {
+  int i,*a,n,j=0,new_number,*prime;
+       n=argc-1;
+  a=(int*)malloc(sizeof(int)*n);
+prime=(int*)malloc(sizeof(int)*n);
+for(i=0;i<n;i++) {
+a[i]=atoi(argv[i+1]);
+if(Isprime(a[i])){
+  prime[j]=a[i];
+  j++;
+  }
+    }
+  selectionSort(a,n);
+  //insertion(a,n);
+  
+  n=j;
+  if(n==0) printf("No prime\n");
+  else{
+  //display(prime,n);
+//  bubbleSort(prime,n); 
+    
+     
+  //display(prime,n);
+    }
+return 0;
+  }
+   
+
+
+
+
+
 
 
